16b5eb8a95456b073cf48f3a4000720417da8d7a
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-04-16 18:44:02 +0300

    Preparatory commit

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,56 @@
+<script setup>
+const props = defineProps({
+  imgUrl:String
+})
+</script>
+<template>
+  <div class="root">
+    <div :style="`background-image:url(${imgUrl})`" class="img">
+
+      </div>
+      <div class="main">
+         <el-button class="btn">Get random cocktail</el-button>
+        <slot></slot>
+      </div>
+
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '../assets/styles/main.scss';
+
+.root{
+  display: flex;
+  background-color: $background;
+  min-height:100vh;
+
+}
+.img{
+  width: 50%;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: cover
+
+}
+.main{
+  position: relative;
+  width: 50%;
+  padding: 32px 40px;
+}
+.btn{
+  position: absolute;
+  top: 32px;
+  right: 40px;
+  font-size: 16px;
+  font-family: 'Raleway','Arial','sans-serif';
+  background-color: $accent;
+  border-color:$accent;
+  color:$text;
+
+  &:active,
+  &:hover{
+    background-color: darken($accent,10%);
+    color: darken($text,10%);
+  }
+}
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,29 @@
 <script setup>
+import AppLayout from '@/components/AppLayout.vue';
 </script>
 
 <template>
-Home
+  <AppLayout imgUrl="/src/assets/img/bg-1.jpg">
+    <div class="wrapper">
+      <div class="info">
+        <div class="title">Choose your drink</div>
+        <div class="line"></div>
+      </div>
+    </div>
+  </AppLayout >
 </template>
 
-<style lang="sass" scoped>
-@import '../assets/styles/main'
+<style lang="scss" scoped>
+@import '../assets/styles/main.scss';
 
-</style>
+.wrapper{
+  display: flex;
+  align-items: center;
+}
+.info{
+  padding: 80px 0;
+  text-align: center;
+  justify-content: center;
+}
+
+</style>import AppLayoutVue from '@/components/AppLayout.vue';
